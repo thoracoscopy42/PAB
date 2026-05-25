f13d2980442966d2fb8c12f5f50bd1b816f9880f
Wiktor Kowalski <wiktorkowalski70@gmail.com>
2026-05-25 21:25:50 +0200

    Produkt dodany do panelu admina

--- project/shopapp/admin.py
+++ project/shopapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Product, Order, Customer, Warehouse
 # Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "description", "stock")
+    search_fields = ("name",)
+    list_filter = ("price",)
