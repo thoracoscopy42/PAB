eaec2df3427db1b579941465c9102bddbc84795f
Wiktor Kowalski <wiktorkowalski70@gmail.com>
2026-05-25 21:26:21 +0200

    dodany stock do modelu produktu, zamiast łączyć tabale będziemy trzymali to tutaj

--- project/shopapp/models.py
+++ project/shopapp/models.py
@@ -7,6 +7,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
+    stock = models.PositiveIntegerField(default=0)
     # product_image = models.ImageField(upload_to='product_images/')
 
     def __str__(self):
