2b6f89f81bd81a228e0e2286d270aeadc11d8c9d
Wiktor Kowalski <wiktorkowalski70@gmail.com>
2026-06-04 19:55:47 +0200

    dodana klasa pośrednia dla pozycji zamówienia - można mieć wiele produktów w jednym

--- project/shopapp/models.py
+++ project/shopapp/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 
 class Product(models.Model):
+
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
@@ -14,25 +15,43 @@ class Product(models.Model):
         return self.name
     
 class Order(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
+
     order_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Order of {self.quantity} {self.product.name}(s) on {self.order_date}" 
+        return f"Order from {self.order_date}" 
     
+class OrderItem(models.Model):
+
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name="items"
+    )
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE
+    )
+
+    quantity = models.PositiveIntegerField()
+
+    #gwarancja unkalności produktów w zamówieniu
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["order", "product"],
+                name="unique_product_per_order"
+            )
+        ]
+
 class Customer(models.Model):
+
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    orders = models.ManyToManyField(Order)
+    orders = models.ManyToManyField(Order, blank=True)
 
     def __str__(self):
         return self.name
 
-class Warehouse(models.Model):
-    location = models.CharField(max_length=100)
-    products = models.ManyToManyField(Product)
-
-    def __str__(self):
-        return self.location
 
