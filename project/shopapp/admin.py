f6ee7692004874df1b5b81c133ecc510ae65fac3
Wiktor Kowalski <wiktorkowalski70@gmail.com>
2026-06-04 20:23:56 +0200

    tworzenie orderów ze strony admina

--- project/shopapp/admin.py
+++ project/shopapp/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 
-from .models import Product, Order, Customer
+from .models import Product, Order, OrderItem, Customer
 # Register your models here.
 
 
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemInline]
+
+
+
 admin.site.register(Product)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem)
 admin.site.register(Customer)
