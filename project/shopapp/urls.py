d408b352c5bf73eacf9586fb784801d3e071c45b
Wiktor Kowalski <wiktorkowalski70@gmail.com>
2026-06-06 19:25:37 +0200

    wizualna zmiana do urlsów

--- project/shopapp/urls.py
+++ project/shopapp/urls.py
@@ -12,9 +12,5 @@ urlpatterns = [
     #dodawanie produktu do koszyka
     path("cart/add/<int:product_id>/", views.add_to_cart, name="add_to_cart"),
     #zmiana ilości w koszyku
-    path(
-    "cart/change/<int:product_id>/<str:action>/",
-    views.change_cart_item,
-    name="change_cart_item"
-    ),
+    path("cart/change/<int:product_id>/<str:action>/", views.change_cart_item, name="change_cart_item"),
 ]
